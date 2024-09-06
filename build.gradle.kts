529e11c8ab1a9a74d89eb651fd70b427b99e115a
jeremy-drmio <jeremy@dremio.com>
2024-09-06 10:43:42 -0700

    Add simple prepared statement execution using arrow flight jdbc driver

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,7 @@ repositories {
 
 dependencies {
     implementation("org.apache.arrow:flight-sql-jdbc-driver:17.0.0")
+    implementation("com.google.code.gson:gson:2.10")
 
     testImplementation(platform("org.junit:junit-bom:5.10.3"))
     testImplementation("org.junit.jupiter:junit-jupiter")
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,68 @@
 package org.example;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Properties;
+import java.util.stream.IntStream;
+
 public class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello world!");
+  public static void main(String[] args) throws Exception {
+
+    Properties props = new Properties();
+    props.setProperty("user", "dremio");
+    props.setProperty("password", "dremio123");
+
+    String connectionStr = "jdbc:arrow-flight-sql://localhost:32010/?useEncryption=false";
+    String query = "select * from Samples.\"samples.dremio.com\".\"zips.json\" limit 5";
+
+    try (Connection con = DriverManager.getConnection(connectionStr, props);
+        PreparedStatement ps = con.prepareStatement(query);
+        ResultSet rs = ps.executeQuery()) {
+
+      System.out.println(getResultsAsJson(rs));
+    }
+  }
+
+  private static String getResultsAsJson(ResultSet rs) throws Exception {
+    ResultSetMetaData md = rs.getMetaData();
+    int numCols = md.getColumnCount();
+    List<String> colNames =
+        IntStream.range(0, numCols)
+            .mapToObj(
+                i -> {
+                  try {
+                    return md.getColumnName(i + 1);
+                  } catch (SQLException e) {
+                    e.printStackTrace();
+                    return "?";
+                  }
+                })
+            .toList();
+
+    JsonArray result = new JsonArray();
+    while (rs.next()) {
+      JsonObject row = new JsonObject();
+      colNames.forEach(
+          cn -> {
+            try {
+              row.addProperty(cn, rs.getObject(cn).toString());
+            } catch (SQLException e) {
+              e.printStackTrace();
+            }
+          });
+      result.add(row);
+    }
+
+    Gson gson = new GsonBuilder().setPrettyPrinting().create();
+    return gson.toJson(result);
   }
 }
